6a2a2824f7ab0080d26119fbf916e5a0c2237e43
Hirodropus <hirodropus@gmail.com>
2025-10-23 09:42:55 +0500

    modified files

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,42 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def get_recipe(request, dish_name):
+    """
+    Обработчик для отображения рецепта блюда
+    """
+    # Получаем рецепт из DATA по названию блюда
+    recipe = DATA.get(dish_name)
+
+    if recipe:
+        # Создаем копию рецепта для корректировки количеств
+        adjusted_recipe = recipe.copy()
+
+        # Проверяем параметр servings из GET-запроса
+        servings_param = request.GET.get('servings')
+
+        if servings_param:
+            try:
+                # Конвертируем в число и проверяем, что оно положительное
+                servings = int(servings_param)
+                if servings > 0:
+                    # Умножаем количество каждого ингредиента на servings
+                    for ingredient in adjusted_recipe:
+                        adjusted_recipe[ingredient] *= servings
+            except (ValueError, TypeError):
+                # Если servings не число, используем исходный рецепт
+                pass
+
+        context = {
+            'recipe': adjusted_recipe
+        }
+    else:
+        # Если рецепт не найден, передаем пустой словарь
+        context = {
+            'recipe': {}
+        }
+
+    return render(request, 'calculator/index.html', context)
